1c1817cf7908af7f5bd0113f07fa5ff6fbcd407d
awojciechowska <awojciechowska@debian.local>
2024-01-29 13:58:53 +0100

    utworzenie skryptu do programu kalkulator

--- calc.py
+++ calc.py
@@ -1,1 +1,24 @@
 # kalkulator
+
+import sys
+
+if len(sys.argv) != 4:
+    print("Użycie: calc.py <liczba_1> <operacja +-> <liczba_2>")
+    sys.exit(1)
+
+liczba_1 = int(sys.argv[1])
+operacja = sys.argv[2]
+liczba_2 = int(sys.argv[3])
+
+if operacja == "+":
+    result = liczba_1 + liczba_2
+elif operacja == "-":
+    result = liczba_1 - liczba_2
+else:
+    print("Niepoprawny operator")
+    sys.exit(1)
+
+with open("/tmp/wynik.txt", "w") as file:
+    file.write( str(result) )
+
+print( result )
